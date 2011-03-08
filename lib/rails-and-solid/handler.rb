0abf31c24999899482387efd3c901fa5a6a8fd79
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-03-08 15:36:39 -0300

    naming issues. its my age...

--- lib/rails-and-solid/handler.rb
+++ lib/rails-and-solid/handler.rb
@@ -1,6 +1,6 @@
 module RailsAndSolid
   module Handler
-    autoload :Key, 'rails-and-solid/handler/key'
+    autoload :KeyHandler, 'rails-and-solid/handler/key_handler'
     autoload :ParamsAndScopes, 'rails-and-solid/handler/params_and_scopes'
     autoload :Load, 'rails-and-solid/handler/load'
     autoload :Param, 'rails-and-solid/handler/param'
