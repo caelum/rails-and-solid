94e7e3f7db057521bb53a4dea8d4f4022073aeba
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-03-08 17:12:12 -0300

    examples for rails and solid

--- lib/rails-and-solid.rb
+++ lib/rails-and-solid.rb
@@ -4,4 +4,9 @@ module RailsAndSolid
   autoload :TrickHim, 'rails-and-solid/trick_him'
   autoload :Handler, 'rails-and-solid/handler'
 
+  # Shortcut to hack rails.
+  def self.pimp(what)
+    eval "class #{what.to_s.camelize}Controller < ApplicationController; end"
+  end
+
 end
