944ee757e7d144bf4a2cb067638137f1e5f658aa
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-03-08 15:04:48 -0300

    just playing

---
+++ lib/rails-and-solid.rb
@@ -0,0 +1,7 @@
+module Rails
+  module And
+    module Solid
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails-and-solid/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module And
+    module Solid
+      VERSION = "0.0.1"
+    end
+  end
+end
